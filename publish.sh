27172237ff66c7fee4ef6b7acb9a4886e599b82e
Konstantin Yegupov <kyegupov4@gmail.com>
2016-10-23 22:35:58 +0100

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -4,8 +4,8 @@ git add --all
 git commit -m "Blog update `date -u +"%Y-%m-%dT%H:%M:%SZ"`" 
 git push
 
-cd public 
-git add --all 
+cd public
+git add --all
 git commit -m "Blog update `date -u +"%Y-%m-%dT%H:%M:%SZ"`" 
 git push
 cd ..
