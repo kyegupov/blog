069ee8e6cfb09841e561566150c7c6f2f46d2100
Konstantin Yegupov <kyegupov4@gmail.com>
2016-10-23 22:33:01 +0100

    Blog update 2016-10-23T21:33:01Z

---
+++ publish.sh
@@ -0,0 +1,12 @@
+set -e
+
+git add --all
+git commit -m "Blog update `date -u +"%Y-%m-%dT%H:%M:%SZ"`" 
+git push
+
+cd public 
+git add --all 
+git commit -m "Blog update `date -u +"%Y-%m-%dT%H:%M:%SZ"`" 
+git push
+cd ..
+
